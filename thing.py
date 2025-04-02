8345d7ed60cdb7d2f93c7acb6c3c0584d17764f9
manasxag <manasag2004@gmail.com>
2025-04-03 01:11:28 +0530

    Add files via upload

--- thing.py
+++ thing.py
@@ -30,8 +30,8 @@ This app predicts the winner of an IPL cricket match based on the current match 
 """)
 
 # Define file paths directly (update these paths according to your files)
-MATCHES_FILE_PATH = r"C:\Users\aniro\Downloads\matches(1).csv"
-DELIVERIES_FILE_PATH = r"C:\Users\aniro\Downloads\deliveries(1).csv"
+MATCHES_FILE_PATH = r"matches(1).csv"
+DELIVERIES_FILE_PATH = r"deliveries(1).csv"
 
 
 # Function to load data
